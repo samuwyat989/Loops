d62a3a3b3907c9fc2fb9afeb01838a69be3ae11a
samuwyat989 <samuwyat989@ed.amdsb.ca>
2016-10-04 15:00:55 -0700

    Add project files.

---
+++ Loops/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Loops
+{
+    partial class backLoop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.begin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // begin
+            // 
+            this.begin.Location = new System.Drawing.Point(101, 226);
+            this.begin.Name = "begin";
+            this.begin.Size = new System.Drawing.Size(75, 23);
+            this.begin.TabIndex = 0;
+            this.begin.Text = "Start";
+            this.begin.UseVisualStyleBackColor = true;
+            this.begin.Click += new System.EventHandler(this.begin_Click);
+            // 
+            // backLoop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.begin);
+            this.Name = "backLoop";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button begin;
+    }
+}
+
---
+++ Loops/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Loops
+{
+    public partial class backLoop : Form
+    {
+        public backLoop()
+        {
+            InitializeComponent();
+        }
+
+        private void begin_Click(object sender, EventArgs e)
+        {
+            Graphics loopLine = this.CreateGraphics();
+            Pen loopPen1 = new Pen(Color.Black, 5);
+            Pen loopPen0 = new Pen(Color.White, 5);
+
+            int loopNumberSide = 180;
+            int loopNumberUp = 95;
+            int x1 = 50;
+            int x2 = 50;
+            int x3 = 230;
+            int x4 = 147;
+            int x5 = 52;
+            
+            for (int x = 0; x < loopNumberSide; x++)
+            {
+                loopLine.DrawLine(loopPen1, x1, 50, x1, 150);
+                Thread.Sleep(1);
+                x1++;
+            }
+
+            Thread.Sleep(1000);
+
+            for (int x = 0; x < loopNumberSide; x++)
+            {
+                loopLine.DrawLine(loopPen0, x2, 50, x2, 150);
+                Thread.Sleep(1);
+                x2++;
+            }
+
+            Thread.Sleep(1000);
+
+            for (int x = 0; x < loopNumberSide; x++)
+            {
+                loopLine.DrawLine(loopPen1, x1, 50, x1, 150);
+                Thread.Sleep(1);
+                x1--;
+            }
+
+            Thread.Sleep(1000);
+
+            for (int x = 0; x < loopNumberSide; x++)
+            {
+                loopLine.DrawLine(loopPen0, x2, 50, x2, 150);
+                Thread.Sleep(1);
+                x2--;
+            }
+
+            Thread.Sleep(1000);
+
+            for (int x = 0; x < loopNumberSide; x++)
+            {
+                loopLine.DrawLine(loopPen1, x1, 50, x1, 55);
+                Thread.Sleep(1);
+                x1++;
+            }
+
+            Thread.Sleep(1);
+
+            for (int x = 0; x < loopNumberUp; x++)
+            {
+                loopLine.DrawLine(loopPen1, 228, x5, 233, x5);
+                Thread.Sleep(1);
+                x5++;
+            }
+
+            Thread.Sleep(1);
+
+            for (int x = 0; x < loopNumberSide; x++)
+            {
+                loopLine.DrawLine(loopPen1, x3, 145, x3, 150);
+                Thread.Sleep(1);
+                x3--;
+            }
+            
+            Thread.Sleep(1);
+
+            for (int x = 0; x < loopNumberUp; x++)
+            {
+                loopLine.DrawLine(loopPen1, 48, x4, 53, x4);
+                Thread.Sleep(1);
+                x4--;
+            }
+                        
+        }
+    }
+}
+
